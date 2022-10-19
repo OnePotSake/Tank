617402025696c8be3c5db17ec33458d571a0a1b7
One pot Sake <hopelamp@sina.com>
2022-10-20 01:05:05 +0800

    1.构建窗体, 窗体坐标从左上角开始0,0. 2.监听键盘事件 keyPressed 获取焦点  keyReleased 释放焦点 3.获取键盘code 用于判断 4.判断键盘焦点code值 是否满足 上下左右

--- tank/src/com/example/MyFrame.java
+++ tank/src/com/example/MyFrame.java
@@ -1,19 +1,24 @@
 package com.example;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MyFrame extends Frame {
 
- int x = 100;
- int y = 100;
+    int x = 100;
+    int y = 100;
 
     public MyFrame() {
         setTitle("test");
         setSize(1024, 768);
+        // 不可改变窗体大小
         setResizable(false);
         setVisible(true);
+
+        this.addKeyListener(new MyKeyLiner());
 
         // 监听 窗体 事件
         // listen in windows event
@@ -27,15 +32,84 @@ public class MyFrame extends Frame {
             }
         });
 
+
     }
 
-    /*clear price  new price*/
+    /**
+     * clear price  new price
+     */
     @Override
     public void paint(Graphics g) {
         g.fillRect(x, y, 50, 50);
-
-        x+=100;
-        y+=100;
-
     }
+
+
+    class MyKeyLiner extends KeyAdapter {
+
+        // 上下左右标识
+        boolean tflag = false;
+        boolean dflag = false;
+        boolean lflag = false;
+        boolean rflag = false;
+
+        /**
+         * 键盘按下获取焦点时
+         */
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // System.out.println("key keyPressed ....");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_UP:
+                    y -= 10;
+                    tflag = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    y += 10;
+                    dflag = true;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    x -= 10;
+                    lflag = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    x += 10;
+                    rflag = true;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        /**
+         * 键盘按下焦点释放后
+         */
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_UP:
+                    y -= 10;
+                    tflag = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    y += 10;
+                    dflag = false;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    x -= 10;
+                    lflag = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    x += 10;
+                    rflag = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
 }
--- tank/src/com/example/RunMyFrame.java
+++ tank/src/com/example/RunMyFrame.java
@@ -1,7 +1,15 @@
 package com.example;
 
 public class RunMyFrame {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MyFrame myFrame = new MyFrame();
+        // 死循环
+        while (true) {
+            // 睡眠1s
+            Thread.sleep(1000);
+            // 画布重新绘制
+            myFrame.repaint();
+        }
+
     }
 }
